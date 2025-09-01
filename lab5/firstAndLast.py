5eb1f22926ed71acc6266071ca55fce001094585
Avani0910 <avani.kavathiya131334@marwadiuniversity.ac.in>
2025-09-01 22:46:21 +0530

    Add files via upload

---
+++ lab5/firstAndLast.py
@@ -0,0 +1,10 @@
+# Define a tuple
+my_tuple = (10, 20, 30, 40, 50)
+
+# Get first and last elements using indexing
+first_element = my_tuple[0]
+last_element = my_tuple[-1]
+
+print("Tuple:", my_tuple)
+print("First element:", first_element)
+print("Last element:", last_element)
