2d58ebd75966999a6902077678ba024a92b407dc
Avani0910 <avani.kavathiya131334@marwadiuniversity.ac.in>
2025-09-06 23:32:18 +0530

    Add files via upload

---
+++ signal_ICT_Avani_Kavathiya_92400133104/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="signal_ICT_AvaniKavathiya_92400133104",
+    version="0.1",
+    description="Custom Signal Generation and Operations Package for Python Exam",
+    author="Avani Kavathiya",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib"
+    ],
+)
